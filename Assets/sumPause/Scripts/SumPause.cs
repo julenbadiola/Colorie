02141fa8bd1608933596675a069f95099de95523
Julen Badiola <33113693+julenbadiola@users.noreply.github.com>
2021-01-03 10:54:19 +0100

    Video Tutorial

---
+++ Assets/sumPause/Scripts/SumPause.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+[RequireComponent (typeof (Image))]
+/// <summary>Singleton class for controlling pause functions.</summary>
+
+public class SumPause : MonoBehaviour {
+
+    // Event managers
+    public delegate void PauseAction (bool paused);
+    public static event PauseAction pauseEvent;
+
+    [SerializeField]
+    public static VideoPlayer videoPlayer;
+    // Variables set via inspector
+    [SerializeField]
+    bool useEvent = false, detectEscapeKey = true;
+    [SerializeField]
+    Sprite pausedSprite, playingSprite;
+
+    // Link to button's image
+    Image image;
+
+    static bool status = false;
+    /// <summary>
+    /// Sets/Returns current pause state (true for paused, false for normal)
+    /// </summary>
+    public static bool Status {
+        get { return status; }
+        set {
+            status = value;
+            Debug.Log ("Pause status set to " + status.ToString ());
+
+            OnChange ();
+
+            // Change image to the proper sprite if everything is set
+            if (CheckLinks ())
+                instance.image.sprite = status ? instance.pausedSprite : instance.playingSprite;
+            else
+                Debug.LogError ("Links missing on SumPause component. Please check the sumPauseButton object for missing references.");
+
+            // Notify other objects of change
+            if (instance.useEvent && pauseEvent != null)
+                pauseEvent (status);
+        }
+    }
+
+    // Instance used for singleton
+    public static SumPause instance;
+
+    void Awake () {
+        image = GetComponent<Image> ();
+    }
+
+    void Start () {
+        videoPlayer = GameObject.Find("VideoPlayer").GetComponent<VideoPlayer>();
+        // Ensure singleton
+        if (SumPause.instance == null)
+            SumPause.instance = this;
+        else
+            Destroy (this);
+    }
+
+    void Update () {
+        // Listen for escape key and pause if needed
+        if (detectEscapeKey && Input.GetKeyDown (KeyCode.Escape))
+            TogglePause ();
+    }
+
+    /// <summary>
+    /// Flips the current pause status. Called from the attached button in 
+    /// the inspector.
+    /// </summary>
+    public void TogglePause () {
+        Status = !Status; // Flip current status
+    }
+
+    /// <summary>Checks if all links are properly connected</summary>
+    /// <returns>False means links are missing</returns>
+    static bool CheckLinks () {
+        return (instance.image != null && instance.playingSprite != null && instance.pausedSprite != null);
+    }
+
+    /// <summary>This is what we want to do when the game is paused or unpaused.</summary>
+    static void OnChange () {
+        if (status) {
+            // What to do when paused
+            videoPlayer.Pause();
+        } else {
+            // What to do when unpaused
+            videoPlayer.Play();
+        }
+    }
+
+}
